0cbae9524ccc7ed4001d39ab19778d00a13f7a55
varun <varunshinde04vrn@gmail.com>
2025-09-24 18:17:26 +0530

    Connected database

--- Backend/server.js
+++ Backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import connectDB from './DataBase/db.js';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -6,6 +7,9 @@ const PORT = process.env.PORT || 5000;
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
+
+// Database connection
+connectDB()
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
