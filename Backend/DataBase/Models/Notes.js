2464bfd8f3d60fc03d5bdad53862a6d755242331
varun <varunshinde04vrn@gmail.com>
2025-09-25 22:42:58 +0530

    added database models

---
+++ Backend/DataBase/Models/Notes.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const notesSchema = new mongoose.Schema({
+    user_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    title: { 
+        type: String,
+        required: true 
+    },
+    description: { 
+        type: String, 
+        default: "",
+        trim: true
+    }
+},
+{timestamps: true})
+
+const Notes = mongoose.model("Notes", notesSchema);
+export default Notes;
