32b6c61e48b4d037a67916cdd4fc1d0326b73a9c
varun <varunshinde04vrn@gmail.com>
2025-09-25 22:44:33 +0530

    updated backend

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,14 @@
 import express from 'express';
 import connectDB from './DataBase/db.js';
+import userRouter from './Routes/userRoutes.js';
+import notesRouter from './Routes/notesRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use('/auth', userRouter);
+app.use('/notes', notesRouter);
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
